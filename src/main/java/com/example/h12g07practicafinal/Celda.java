0a5211f33cbe99ddbf37532f5ca8d08e6b2cf048
diego-avendano <diego.avendano@edu.uah.es>
2026-05-17 13:58:15 +0200

    1

--- src/main/java/com/example/h12g07practicafinal/Celda.java
+++ src/main/java/com/example/h12g07practicafinal/Celda.java
@@ -1,13 +1,63 @@
 package com.example.h12g07practicafinal;
 
 public class Celda {
-    private int fila, columna;
+    private int fila;
+    private int columna;
+    private boolean visitado;
+    private Celda[] vecinos;
+    private int numVecinos;
     private Objeto objeto;
     private Enemigo enemigo;
     private boolean trampa;
+
+    public Celda (int fila, int columna) {
+        this.fila = fila;
+        this.columna = columna;
+        this.visitado = false;
+        this.vecinos = new Celda[4];
+        this.numVecinos = 0;
+        this.objeto = null;
+        this.enemigo = null;
+        this.trampa = false;
+    }
+
+    public void agregarVecino (Celda vecino) {
+        if (numVecinos < 4) {
+            vecinos[numVecinos] = vecino;
+            numVecinos++;
+        }
+    }
+
+    public Celda[] obtenerVecinos() {
+        Celda[] resultado = new Celda[numVecinos];
+        for (int i = 0; i < numVecinos; i++) {
+            resultado[i] = vecinos[i];
+        }
+        return resultado;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public boolean isVisitado() {
+        return visitado;
+    }
+
+    public void setVisitado(boolean visitado) {
+        this.visitado = visitado;
+    }
 
     public boolean isEmpty() {return objeto == null;}
     public boolean hayObjeto() {return objeto != null;}
     public boolean hayEnemigo() {return enemigo != null;}
     public boolean hayTrampa() {return trampa;}
+
+    public String toString() {
+        return "Celda(" + fila + ", " + columna + ")";
+    }
 }
--- src/main/java/com/example/h12g07practicafinal/Habitacion.java
+++ src/main/java/com/example/h12g07practicafinal/Habitacion.java
@@ -1,4 +1,46 @@
 package com.example.h12g07practicafinal;
 
-public class Habitacion {
+public class Habitacion extends Celda {
+    private int ancho;
+    private int alto;
+    private Celda[][] cuadricula;
+
+    public Habitacion(int fila, int columna, int ancho, int alto) {
+        super(fila, columna);
+        this.ancho = ancho;
+        this.alto = alto;
+        this.cuadricula = new Celda[ancho][alto];
+    }
+
+    public void construirMatriz() {
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                if (i > 0) cuadricula[i][j].agregarVecino(cuadricula[i-1][j]);
+                if (i < alto -1) cuadricula [i][j].agregarVecino(cuadricula[i+1][j]);
+                if (j > 0) cuadricula[i][j].agregarVecino(cuadricula[i][j-1]);
+                if (j < ancho -1) cuadricula[i][j].agregarVecino(cuadricula[i][j+1]);
+            }
+        }
+    }
+
+    public Celda obtenerCelda(int x, int y) {
+        if (x >= 0 && x < alto && y >= 0 && y < ancho) {
+            return cuadricula[x][y];
+        }
+        return null;
+    }
+
+    public int getAncho () {
+        return ancho;
+    }
+
+    public int getAlto () {
+        return alto;
+    }
+
+    public String toString () {
+        return "Habitacion(" + getFila() + "," + getColumna() + ")";
+    }
+
+
 }
